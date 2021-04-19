80feeecf131f5b1defe7fec10cb865c3c7d36093
LeeJinYu <22yuu@naver.com>
2021-04-19 18:18:38 +0900

    commit

---
+++ php_project/domestic.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<script src="http://code.jquery.com/jquery-1.12.3.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="res/css/common.css">
+	<script type="text/javascript" src="res/js/script.js"></script>
+</head>
+<body>
+
+<div id="header">
+	<div class="logo">
+		<a href="#">TRAVEL</a>
+	</div>
+	<div class="menu-wrap">
+		<ul class="menu">
+			<li >
+				<div style="border-bottom:1px solid #ddd; color: #222; height: 70px;"><a href="#">DOMESTIC</a></div></li>
+			<li><a href="oversea.php">OVERSEA</a></li>
+			<li><a href="login.php">LOGIN</a></li>
+			<li><a href="join.php">JOIN</a></li>		
+			<li><a href="qna.php">QnA</a></li>
+		</ul>
+	</div>
+</div>
+<div>
+</div>
+<div id="imgSlider">
+	<div class="imgs">
+		<ul>
+			<li><img src="res/images/slide/domestic/1.jpg"></li>
+			<li><img src="res/images/slide/domestic/2.jpg"></li>
+			<li><img src="res/images/slide/domestic/3.jpg"></li>
+			<li><img src="res/images/slide/domestic/4.jpg"></li>
+			<li><img src="res/images/slide/domestic/5.jpg"></li>
+		</ul>
+	</div>	
+</div>
+
+
+
+<footer>
+	
+</footer>
+</body>
+</html>
---
+++ php_project/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta>
+	<title></title>
+	<script src="http://code.jquery.com/jquery-1.12.3.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="res/css/main.css">
+	<script type="text/javascript" src="res/js/script.js"></script>
+</head>
+<body>
+<div class="dim"></div>
+<div id="app">
+	<div id="mainSlide">
+		<div class="slideText">
+			<a href="#"><h1>TRAVEL</h1></a>
+			<ul>
+				<li><a href="domestic.php">DOMESTIC</a></li>
+				<li><a href="oversea.php">OVERSEA</a></li>
+			</ul>
+		</div>
+		<ul class="imgs">
+			<li><img src="res/images/main_images/main5.jpg"></li> 
+			<li><img src="res/images/main_images/main4.jpg"></li> 
+	        <li><img src="res/images/main_images/main3.jpg"></li> 
+	        <li><img src="res/images/main_images/main2.jpg"></li> 
+	        <li><img src="res/images/main_images/main1.jpg"></li>
+	    </ul>
+	</div>
+</div>
+</body>
+</html>
